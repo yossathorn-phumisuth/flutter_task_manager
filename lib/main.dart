7cf0a9103e4b12b731e0775385401a31e86232f0
teddy <teddy@test.com>
2025-11-13 15:11:57 +0700

    config: Untrack generated files after adding to gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter fdfd ',
+      title: 'Flutter 2025 Demo',
       theme: ThemeData(
         // This is the theme of your application.
         //
